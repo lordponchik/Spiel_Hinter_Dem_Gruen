48cf00eb4e8820fa56b95b0c45059f6e1ed883b0
Denys Strashniuk <d.strashniuk@gmail.com>
2025-05-12 14:09:32 +0200

    hinzufügen: structur des projektes

---
+++ Spiel_Hinter_Dem_Gruen/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Spiel_Hinter_Dem_Gruen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
