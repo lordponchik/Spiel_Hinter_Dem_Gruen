ca0090e0c5397959340e8f24385c8731cf482f36
Denys Strashniuk <d.strashniuk@gmail.com>
2025-05-12 16:11:18 +0200

    hinzufügen: Klasse Startbildschirm

---
+++ Spiel_Hinter_Dem_Gruen/Startbildschirm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spiel_Hinter_Dem_Gruen
+{
+    class Startbildschirm : SpielNameBasis
+    {
+        protected override void SetPositionSpielName(int anzahlIntroZeichen, int anzahlIntroZeilen, int yPosIndex)
+        {
+            int xPos = (Console.WindowWidth - anzahlIntroZeichen) / 2;
+            int yPos = (Console.WindowHeight / 2) - (anzahlIntroZeilen / 2) + yPosIndex;
+
+            Console.SetCursorPosition(xPos, yPos);
+        }
+
+        protected override void SetPositionExtraText(int anzahlIntroZeichen, int anzahlIntroExtraZeichen)
+        {
+            int xPos = (Console.WindowWidth - anzahlIntroZeichen) / 2 + (anzahlIntroZeichen - anzahlIntroExtraZeichen);
+            int yPos = Console.GetCursorPosition().Top + 2;
+
+            Console.SetCursorPosition(xPos, yPos);
+        }
+    }
+}
