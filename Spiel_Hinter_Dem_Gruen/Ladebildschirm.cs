32d56ffb7be732ca5a6e492bac9ac89021afc293
Denys Strashniuk <d.strashniuk@gmail.com>
2025-05-12 14:53:31 +0200

    hinzufügen: Klasse Ladebildschirm

---
+++ Spiel_Hinter_Dem_Gruen/Ladebildschirm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spiel_Hinter_Dem_Gruen
+{
+    class Ladebildschirm
+    {
+        private static string _ladenText = "L A D E N . . .";
+        public static void ZeigeLadebildschirm(int wiederholungen = 1)
+        {
+            Console.Clear();
+            int x = Console.WindowWidth / 2 - _ladenText.Length / 2;
+            int y = Console.WindowHeight / 2;
+
+            while (wiederholungen > 0)
+            {
+                Console.SetCursorPosition(x, y);
+
+                for (int i = 0; i < _ladenText.Length; i++)
+                {
+                    Console.Write(_ladenText[i]);
+                    Thread.Sleep(50);
+                }
+
+                Thread.Sleep(1000);
+
+                Console.SetCursorPosition(x, y);
+
+                for (int i = 0; i < _ladenText.Length; i++)
+                {
+                    Console.Write(" ");
+                    Thread.Sleep(50);
+                }
+
+                wiederholungen--;
+            }
+
+            Console.Clear();
+        }
+    }
+}
