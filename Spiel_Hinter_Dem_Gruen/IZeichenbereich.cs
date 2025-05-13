67c31c3730e56cf8e7fbbb352f1f78836ac6cc92
Denys Strashniuk <d.strashniuk@gmail.com>
2025-05-13 12:39:03 +0200

    hinzufügen: Interface IZeichenbereich

---
+++ Spiel_Hinter_Dem_Gruen/IZeichenbereich.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spiel_Hinter_Dem_Gruen
+{
+    interface IZeichenbereich
+    {
+        public static int StartX { get; }
+        public static int StartY { get; }
+
+        public static int EndeX { get; }
+        public static int EndeY { get; }
+
+        public static int AktuellX { get; set; }
+
+        public static int AktuellY { get; set; }
+    }
+}
