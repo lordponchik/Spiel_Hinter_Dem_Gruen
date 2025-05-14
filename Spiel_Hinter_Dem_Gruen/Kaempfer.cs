96c2a64619e9c69f8f1649394bde4da3fd4e3b8e
Denys Strashniuk <d.strashniuk@gmail.com>
2025-05-14 08:16:14 +0200

    hinzufügen: abstract Klasse Kaempfer

---
+++ Spiel_Hinter_Dem_Gruen/Kaempfer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spiel_Hinter_Dem_Gruen
+{
+    abstract class Kaempfer
+    {
+        public string Name { get; set; }
+        public int Leben { get; protected set; } = 100;
+
+        public int KoerperTeilAngriff { get; protected set; }
+        public int? KoerperTeilVerteidigung { get; protected set; }
+
+        public abstract void WaehleAngriff();
+        public virtual void WaehleVerteidigung()
+        {
+            KoerperTeilVerteidigung = null;
+        }
+        public virtual void ErhalteSchaden(int schaden)
+        {
+            Leben -= schaden;
+            if (Leben < 0) Leben = 0;
+        }
+
+        public bool IstBesiegt()
+        {
+            return Leben <= 0;
+        }
+    }
+}
