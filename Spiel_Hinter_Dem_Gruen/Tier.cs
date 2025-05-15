b5110a52b8b071b4620f858ea0e3762e7648d061
Denys Strashniuk <d.strashniuk@gmail.com>
2025-05-15 10:55:26 +0200

    umbennen: Koerperteile und korrigieren: Konstruktor Tier

--- Spiel_Hinter_Dem_Gruen/Tier.cs
+++ Spiel_Hinter_Dem_Gruen/Tier.cs
@@ -8,12 +8,11 @@ namespace Spiel_Hinter_Dem_Gruen
 {
     class Tier :Kaempfer
     {
-        private static readonly string[] Koerperteile = { "Kopf", "Brust", "Beine" };
+        private static readonly string[] Koerperteile = { "Kopf", "Rumpf", "Beine" };
         private static Random rand = new Random();
 
-        public Tier(string name, int leben) {
-            Name = name;
-            Leben = leben;
+        public Tier(string name, int leben):base(name, leben) {
+
         }
         public override void WaehleAngriff()
         {
