43f62d3f983d7793400a4046974a3db25e8cdf40
Denys Strashniuk <d.strashniuk@gmail.com>
2025-05-15 08:24:15 +0200

    anpassen: Methode EinstellenInteraktivesMenue und hinzufügen: Methode Reset

--- Spiel_Hinter_Dem_Gruen/Fussbereich.cs
+++ Spiel_Hinter_Dem_Gruen/Fussbereich.cs
@@ -10,15 +10,22 @@ namespace Spiel_Hinter_Dem_Gruen
     {
         private static int _aktuellX = 0;
         private static int _aktuellY = Console.WindowHeight / 2 + 1;
-        public static  int AktuellX { get { return _aktuellX; } set { _aktuellX = value; } }
-        public static  int AktuellY { get { return _aktuellY; } set { _aktuellY = value; } }
+        public static int AktuellX { get { return _aktuellX; } set { _aktuellX = value; } }
+        public static int AktuellY { get { return _aktuellY; } set { _aktuellY = value; } }
         public static int StartX { get { return 0; } }
         public static int StartY { get { return Console.WindowHeight / 2 + 1; } }
 
         public static int EndeX { get { return Console.WindowWidth / 3 * 2 - 1; } }
         public static int EndeY { get { return Console.WindowHeight; } }
-        public static void EinstellenInteraktivesMenue(string[] punkte, int auswahl)
+        public static void EinstellenInteraktivesMenue(string[] punkte, int auswahl, string text = "")
         {
+            if (text.Length != 0)
+            {
+                Console.SetCursorPosition(_aktuellX, _aktuellY);
+                Console.WriteLine(text);
+                AktuellY += 1;
+            }
+
             for (int i = 0; i < punkte.Length; i++)
             {
 
@@ -33,6 +40,21 @@ namespace Spiel_Hinter_Dem_Gruen
                 Console.ForegroundColor = ConsoleColor.White;
             }
 
+            AktuellY = Console.WindowHeight / 2 + 1;
+        }
+
+
+        public static void Reset()
+        {
+            int bereichBreite = EndeX - StartX;
+
+            for (int y = StartY; y < EndeY; y++)
+            {
+                Console.SetCursorPosition(StartX, y);
+                Console.Write("".PadRight(bereichBreite));
+            }
+
+            Console.SetCursorPosition(StartX, StartY);
         }
 
     }
