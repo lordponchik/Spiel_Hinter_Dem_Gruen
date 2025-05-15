d38dd71242c0c8e86e8f732541743e2b9a9682ef
Denys Strashniuk <d.strashniuk@gmail.com>
2025-05-15 11:46:00 +0200

    hinzufügen: zur Dictionary gebietBeschreibungen und Gegner; hinzufügen: Methoden ZeigeGebietBeschreibungen und ZeigeGebietGegner

--- Spiel_Hinter_Dem_Gruen/Weltkarte.cs
+++ Spiel_Hinter_Dem_Gruen/Weltkarte.cs
@@ -12,7 +12,9 @@ namespace Spiel_Hinter_Dem_Gruen
     {
         public string AktuellerOrtId { get; set; }
 
-        Dictionary<string, Szene> weltkarte = new Dictionary<string, Szene>
+        public readonly string[] _ortListe = ["ork_1", "ork_2", "ork_3"];
+
+        private Dictionary<string, Szene> _weltkarte = new Dictionary<string, Szene>
         {
             {
                 "ork_1",
@@ -21,15 +23,45 @@ namespace Spiel_Hinter_Dem_Gruen
                 {
                     Titel = "Kapitel 1.1",
                     Name = "Höhlen der Verstoßenen",
-                    Beschreibung= new List<string>{"Die Luft ist feucht und schwer, durchdrungen vom beißenden Gestank von Schweiß, altem Fleisch und nassem Stein.\n", 
+                    Beschreibung= new List<string>{"Die Luft ist feucht und schwer, durchdrungen vom beißenden Gestank von Schweiß, altem Fleisch und nassem Stein.\n",
                         "Seit unzähligen Nächten schläfst du hier, eingerollt auf einem zerfetzten Fell zwischen Knochen, Fackelruß und Grunzlauten deiner Artgenossen.\n",
-                        "In der Dunkelheit verlieren selbst Orks ihr Zeitgefühl – doch heute reißt dich etwas anderes aus dem Halbschlaf.\n", 
+                        "In der Dunkelheit verlieren selbst Orks ihr Zeitgefühl – doch heute reißt dich etwas anderes aus dem Halbschlaf.\n",
                         "\n",
                         "Ein schabendes, feuchtes Geräusch. Erst undeutlich, dann scharf. Du öffnest die Augen.\n",
                 "Kleine, scharfe Zähne graben sich genüsslich in deinen Zeh.\n",
                 "Eine fette Ratte sitzt dort, starrt dich mit leeren Augen an und kaut ungerührt weiter.\n",
                 "\n",
                 "Knusper, knabber... chrum-chrum.\n"},
+                    gebietBeschreibungen = new List<string>[]
+                    {
+                       new List<string> {"Die Pilzschlucht\n", "\n","Ein feuchter, modriger Pfad, der von riesigen leuchtenden Pilzen gesäumt ist.\n",
+                            "Der Gestank von Schimmel liegt in der Luft.\n",
+                            "Ein Ork steht mitten auf dem Pfad, seine Keule dampft von frischem Blut.\n" },
+                        new List<string> {"Der Knochenplatz\n", "\n","Knochen türmen sich zu kleinen Haufen, als hättest du ein Schlachtfeld längst vergangener Kämpfe betreten.\n",
+                             "Ein breitschultriger Ork thront auf einem umgestürzten Thron aus Rippen.\n" },
+                        new List<string> {"Die Feuerspalte\n","\n","Ein schmaler Gang führt an einer glühenden Felsspalte vorbei, aus der Hitze und Asche aufsteigen.\n",
+                             "Ein tätowierter Ork wirbelt mit flammenden Sicheln durch die Luft.\n" },
+                        new List<string>   {"Der Tränensee\n","\n","Stilles Wasser sammelt sich in einer Senke.\n",
+                             "Kristalle wachsen an den Wänden.\n",
+                           "Die Luft ist still – zu still.",
+                           "Ein Ork mit bemaltem Gesicht erhebt sich langsam aus dem Wasser."},
+                          new List<string> {"Die Eisenkluft\n", "\n","Rostige Waffen, zerbrochene Rüstungen – hier wurden viele besiegt.\n",
+                             "Ein schwer gerüsteter Ork mit einem gigantischen Hammer wartet schweigend.\n"  },
+                          new List<string> {"Der Ausstiegsschacht\n", "\n","Endlich – das Licht!\n",
+                             "Ein natürlicher Schacht öffnet sich nach oben. \n",
+                           "Doch zwischen dir und dem Aufstieg steht der größte Ork, den du je gesehen hast.\n",
+                            },
+
+                    },
+                     Gegner = new List<Kaempfer>
+                          {
+                            new Gegner("Grubnag der Gierige", 20, new List<string>{ "Grubnag der Gierige\n", "\n", "Du stinkst nicht besser als die Ratte. Mal sehen, wie du schmeckst!\n", "Grubnag will dein Herz... zum Frühstück!\n" } ),
+                            new Gegner("Brakzahn der Brutale", 25, new List<string>{ "Brakzahn der Brutale\n", "\n", "Ich hab mit einem Finger mehr Gegner zerschmettert, als du Haare aufm Kopf hast!\n", "Komm näher, Kleiner. Ich will sehen, wie schnell du blutest.\n" }),
+                            new Gegner("Xargash Flammenatem", 30, new List<string>{ "Xargash Flammenatem\n", "\n", "Feuer reinigt. Bist du bereit für die Flammen?\n", "Ich werde dich grillen – schön knusprig!\n" }),
+                            new Gegner("Mok der Verstummte", 35, new List<string>{ "Mok der Verstummte\n", "\n", "(Er sagt kein Wort. Nur ein knurrender Laut. Dann ein tiefer Atemzug.)\n", "(Er zeigt stumm auf deine Kehle – und macht eine schneidende Geste.)\n" }),
+                            new Gegner("Kror Eisenfaust", 40, new List<string>{ "Kror Eisenfaust\n", "\n", "Eisen redet nicht. Es zerschmettert.\n", "Deine Knochen? Nur neue Trophäen für meine Wand.\n" }),
+                            new Gegner("Drog’Marr der Wächter", 45, new List<string>{ "Drog’Marr der Wächter\n", "\n", "Ich habe hunderte kommen sehen... keiner ging.\n", "Beweise, dass du mehr bist als nur ein weiterer Verstoßener.\n" }),
+                          }
                 }
             },
             {   "ork_2",
@@ -57,12 +89,11 @@ namespace Spiel_Hinter_Dem_Gruen
             AktuellerOrtId = aktuellerOrtId;
         }
 
-        public void ZeigeKapitel()
+        public void ZeigeKapitel(string aktuellerOrtId)
         {
             Console.Clear();
 
-            
-            Szene aktuelleSzene = weltkarte[AktuellerOrtId];
+            Szene aktuelleSzene = _weltkarte[aktuellerOrtId];
 
             List<string> gesamtText = new List<string>();
             gesamtText.AddRange(new List<string> { $"{aktuelleSzene.Titel}\n", "\n", $"Ort: {aktuelleSzene.Name}\n", "\n" });
@@ -79,6 +110,20 @@ namespace Spiel_Hinter_Dem_Gruen
                 key = Console.ReadKey(true).Key;
             }
             while (key != ConsoleKey.Enter);
+
+        }
+        public List<string>[] ZeigeGebietBeschreibung(string aktuellerOrtId)
+        {
+            Szene aktuelleSzene = _weltkarte[aktuellerOrtId];
+
+            return aktuelleSzene.gebietBeschreibungen;
+
+        }
+        public List<Kaempfer> ZeigeGebietGegner(string aktuellerOrtId)
+        {
+            Szene aktuelleSzene = _weltkarte[aktuellerOrtId];
+
+            return aktuelleSzene.Gegner;
 
         }
     }
