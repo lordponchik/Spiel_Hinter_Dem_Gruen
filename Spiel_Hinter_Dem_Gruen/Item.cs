1962d64b9345b04f4219f3889b7bcb3a2c135e0e
Denys Strashniuk <d.strashniuk@gmail.com>
2025-05-15 12:55:04 +0200

    hinzufügen: Klasse Item

---
+++ Spiel_Hinter_Dem_Gruen/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spiel_Hinter_Dem_Gruen
+{
+    abstract class Item
+    {
+        public string Name { get; set; }
+        public string Typ { get; set; }
+
+        public int Wert { get; set; }
+
+        public Item(string name, string typ, int wert)
+        {
+            Name = name;
+            Typ = typ;
+            Wert = wert;
+        }
+
+        public abstract Item Klonen();
+    }
+}
