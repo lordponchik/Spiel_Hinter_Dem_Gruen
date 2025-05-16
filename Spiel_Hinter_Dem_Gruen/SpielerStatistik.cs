7cd99324cfff6c0ee6836dc50c58e1316d0d5f7b
Denys Strashniuk <d.strashniuk@gmail.com>
2025-05-16 14:49:49 +0200

    hinzufügen: SpielerStatistik

---
+++ Spiel_Hinter_Dem_Gruen/SpielerStatistik.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spiel_Hinter_Dem_Gruen
+{
+    public static class  SpielerStatistik
+    {
+        public static int Siege { get;set; }
+        public static int VerwendeteHeilmittel
+        {
+            get;set;
+        }
+
+        public static void ErhoeheSiege()
+        {
+            Siege += 1;
+        }
+        public static void VerwendeHeilMittel()
+        {
+            VerwendeteHeilmittel += 1;
+        }
+    }
+}
