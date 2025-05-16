9c8bd16e0e240bdfeea648eb6cc0ad41bd75492f
Denys Strashniuk <d.strashniuk@gmail.com>
2025-05-16 08:39:04 +0200

    hinzufügen: zwei Eigenschaften und Methode VerursachterSchaden

--- Spiel_Hinter_Dem_Gruen/Kaempfer.cs
+++ Spiel_Hinter_Dem_Gruen/Kaempfer.cs
@@ -9,15 +9,22 @@ namespace Spiel_Hinter_Dem_Gruen
     abstract class Kaempfer
     {
         public string Name { get; set; }
+        private static Random _zufall = new Random();
         public int Leben { get; protected set; } = 100;
+
+        public Waffe? AktiveWaffe { get; protected set; }
+
+        public int Schaden { get; protected set; }
 
         public int KoerperTeilAngriff { get; protected set; }
         public int? KoerperTeilVerteidigung { get; protected set; }
 
-        public Kaempfer(string name, int leben)
+        public Kaempfer(string name, int leben, int schaden, Waffe? aktiveWaffe = null)
         {
             Name = name;
             Leben = leben;
+            Schaden = schaden;
+            AktiveWaffe = aktiveWaffe;
         }
 
         public abstract void WaehleAngriff();
@@ -36,6 +43,18 @@ namespace Spiel_Hinter_Dem_Gruen
             return Leben <= 0;
         }
 
+        public int VerursachterSchaden()
+        {
+
+            int extraSchaden = 0;
+
+            if (AktiveWaffe != null) extraSchaden += AktiveWaffe.Wert;
+
+
+            int geschadet = _zufall.Next(Schaden-2, Schaden + extraSchaden + 1);
+
+            return geschadet;
+        }
         public virtual void Rede() { }
     }
 }
