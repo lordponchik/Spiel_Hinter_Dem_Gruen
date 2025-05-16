5b73c53ad64c752b681f78322e5c9f74eb25a76a
Denys Strashniuk <d.strashniuk@gmail.com>
2025-05-16 12:44:33 +0200

    umbennen: Eigenschaft

--- Spiel_Hinter_Dem_Gruen/Tier.cs
+++ Spiel_Hinter_Dem_Gruen/Tier.cs
@@ -6,17 +6,34 @@ using System.Threading.Tasks;
 
 namespace Spiel_Hinter_Dem_Gruen
 {
-    class Tier :Kaempfer
+    class Tier : Kaempfer
     {
-        private static readonly string[] Koerperteile = { "Kopf", "Rumpf", "Beine" };
-        private static Random rand = new Random();
-
-        public Tier(string name, int leben, int schaden):base(name, leben, schaden) {
-
+        private static readonly string[] _koerperteile = { "Kopf", "Rumpf", "Beine" };
+        private static Random _zufall = new Random();
+        private List<Item> _auszeichnung = new List<Item>();
+        public List<Item> Auszeichnung { get { return _auszeichnung; } }
+        public Tier(string name, int leben, int schaden) : base(name, leben, schaden)
+        {
+            AuszeichnungenHinzufuegen();
         }
         public override void WaehleAngriff()
         {
-            KoerperTeilAngriff = rand.Next(0, Koerperteile.Length);
+            KoerperTeilAngriff = _zufall.Next(0, _koerperteile.Length);
+        }
+
+
+        public void AuszeichnungenHinzufuegen()
+        {
+            _auszeichnung.Clear();
+
+            if (AktiveWaffe != null) _auszeichnung.Add(AktiveWaffe);
+
+            int heilmittelAnzahl = _zufall.Next(1, 5);
+
+            for (int i = 0; i < heilmittelAnzahl; i += 1)
+            {
+                _auszeichnung.Add(ItemDatenbank.zitterpilz);
+            }
         }
     }
 }
