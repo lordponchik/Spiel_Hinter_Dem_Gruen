a1fa0863be7dad963fe1c47a5073e751b212d5f5
Denys Strashniuk <d.strashniuk@gmail.com>
2025-05-16 08:14:44 +0200

    hinzufügen: Klasse Waffe

---
+++ Spiel_Hinter_Dem_Gruen/Waffe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spiel_Hinter_Dem_Gruen
+{
+    class Waffe : Item
+    {
+
+
+        public Waffe(string name,int schaden) : base(name, "Waffe", schaden)
+        {
+        }
+
+        public override Item Klonen()
+        {
+            return new Waffe(Name, Wert);
+        }
+    }
+}
