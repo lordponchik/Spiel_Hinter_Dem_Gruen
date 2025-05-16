a32fa94c2134f0547c3e5a7a85f3d931551b1e19
Denys Strashniuk <d.strashniuk@gmail.com>
2025-05-16 08:23:23 +0200

    hinzufügen: Klasse ItemDatenbank

---
+++ Spiel_Hinter_Dem_Gruen/ItemDatenbank.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spiel_Hinter_Dem_Gruen
+{
+    static class ItemDatenbank
+    {
+        public static readonly Heilmittel zitterpilz = new Heilmittel("Zitterpilz", minHeilung: 5, maxHeilung: 15, anzahl: 1);
+
+        public static readonly Waffe knorpelknacker = new Waffe("Knorpelknacker", schaden: 5);
+
+        public static readonly Waffe rostklaue = new Waffe("Rostklaue", schaden: 5);
+    }
+}
