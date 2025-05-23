641fa518fecf01c75debb7085336ae41ee688260
Denys Strashniuk <d.strashniuk@gmail.com>
2025-05-23 19:05:06 +0200

    refaktorieren: Klasse Seitenbereich

--- Spiel_Hinter_Dem_Gruen/UI/Seitenbereich.cs
+++ Spiel_Hinter_Dem_Gruen/UI/Seitenbereich.cs
@@ -7,43 +7,20 @@ using System.Threading.Tasks;
 
 namespace Spiel_Hinter_Dem_Gruen.UI
 {
-    class Seitenbereich : IZeichenbereich
+    class Seitenbereich : ZeichenbereichBase
     {
-        private static int _aktuellX = Console.WindowWidth / 3 * 2 + 1;
-        private static int _aktuellY = 0;
+        private int _aktuellX = Console.WindowWidth / 3 * 2 + 1;
+        private int _aktuellY = 0;
 
-        public static int StartX { get { return Console.WindowWidth / 3 * 2 + 1; } }
-        public static int StartY { get { return 0; } }
+        public override int StartX { get { return Console.WindowWidth / 3 * 2 + 1; } }
+        public override int StartY { get { return 0; } }
 
-        public static int EndeX { get { return Console.WindowWidth; } }
-        public static int EndeY { get { return Console.WindowHeight; } }
+        public override int EndeX { get { return Console.WindowWidth - 1; } }
+        public override int EndeY { get { return Console.WindowHeight - 1; } }
 
-        public static int AktuellX { get { return _aktuellX; } set { _aktuellX = value; } }
+        public override int AktuellX { get { return _aktuellX; } set { _aktuellX = value; } }
 
-        public static int AktuellY { get { return _aktuellY; } set { _aktuellY = value; } }
+        public override int AktuellY { get { return _aktuellY; } set { _aktuellY = value; } }
 
-        public static void EinstellenAusgabeInformation(string text)
-        {
- 
-            Console.SetCursorPosition(AktuellX, AktuellY);
-            Console.Write(text);
-
-            AktuellY += 1;
-        }
-        public static void Reset()
-        {
-            AktuellX = StartX;
-            AktuellY = StartY;
-            Console.SetCursorPosition(AktuellX, AktuellY);
-
-
-            for (int i = 0; i < EndeY; i++)
-            {
-                EinstellenAusgabeInformation("                                                            ");
-            }
-            AktuellX = StartX;
-            AktuellY = StartY;
-           
-        }
     }
 }
