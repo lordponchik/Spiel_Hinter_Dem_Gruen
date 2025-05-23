3af90bdd8e415ce099c36f12491a876851d6cf81
Denys Strashniuk <d.strashniuk@gmail.com>
2025-05-23 19:05:37 +0200

    refaktorieren: Klasse ZentrierterBereich

--- Spiel_Hinter_Dem_Gruen/UI/ZentrierterBereich.cs
+++ Spiel_Hinter_Dem_Gruen/UI/ZentrierterBereich.cs
@@ -6,54 +6,17 @@ using System.Threading.Tasks;
 
 namespace Spiel_Hinter_Dem_Gruen.UI
 {
-    class ZentrierterBereich : IZeichenbereich
+    class ZentrierterBereich : ZeichenbereichBase
     {
-        private static int _aktuellX;
-        private static int _aktuellY = EndeY / 2;
-        public static int StartX { get { return 0; } }
-        public static int StartY { get { return 0; } }
+        private  int _aktuellX;
+        private  int _aktuellY = Console.WindowHeight / 2;
+        public override int StartX { get { return 0; } }
+        public override int StartY { get { return 0; } }
 
-        public static int EndeX { get { return  Console.WindowWidth; } }
-        public static int EndeY { get { return Console.WindowHeight; } }
+        public override int EndeX { get { return  Console.WindowWidth; } }
+        public override int EndeY { get { return Console.WindowHeight; } }
 
-        public static int AktuellX { get { return _aktuellX; } set { _aktuellX = value; } }
-
-        public static int AktuellY { get { return _aktuellY; } set { _aktuellY = value; } }
-
-        public static void EinstellenInteraktivesMenue(string[] punkte, int auswahl, string text = "")
-        {
-            for (int i = 0; i < punkte.Length; i++)
-            {
-                int xPos = (EndeX - punkte[i].Length) / 2;
-                int yPos = EndeY / 2 + i;
-
-                Console.SetCursorPosition(xPos, yPos);
-
-                if (i == auswahl)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                }
-                Console.WriteLine(punkte[i]);
-
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-
-        }
-        public static void EinstellenAusgabeInformation(List<string> texte)
-        {
-            AktuellY -= texte.Count / 2;
-
-            for (int i = 0; i < texte.Count; i++)
-            {
-                string text = texte[i];
-                int xPos = (EndeX - texte[i].Length) / 2;
-                int yPos = AktuellY + i;
-
-                Console.SetCursorPosition(xPos, yPos);
-                Console.Write(text);
-            }
-
-            AktuellY = EndeY / 2;
-        }
+        public override int AktuellX { get { return _aktuellX; } set { _aktuellX = value; } }
+        public override int AktuellY { get { return _aktuellY; } set { _aktuellY = value; } }
     }
 }
