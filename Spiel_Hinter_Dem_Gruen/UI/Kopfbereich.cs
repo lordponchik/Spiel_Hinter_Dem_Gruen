fcab5a1ea4237e617139365f6ee66cb5887dc8be
Denys Strashniuk <d.strashniuk@gmail.com>
2025-05-23 19:04:00 +0200

    refaktorieren: Klasse Kopfbereich

--- Spiel_Hinter_Dem_Gruen/UI/Kopfbereich.cs
+++ Spiel_Hinter_Dem_Gruen/UI/Kopfbereich.cs
@@ -6,47 +6,18 @@ using System.Threading.Tasks;
 
 namespace Spiel_Hinter_Dem_Gruen.UI
 {
-    class Kopfbereich : IZeichenbereich
+    class Kopfbereich : ZeichenbereichBase
     {
-        private static int _aktuellX = 0;
-        private static int _aktuellY = EndeY - 1;
-        public static int StartX { get { return 0; } }
-        public static int StartY { get { return 0; } }
+        private int _aktuellX = 0;
+        private int _aktuellY = 0;
+        public override int StartX { get { return 0; } }
+        public override int StartY { get { return 0; } }
 
-        public static int EndeX { get { return Console.WindowWidth / 3 * 2 - 1; } }
-        public static int EndeY { get { return Console.WindowHeight / 2 - 1; } }
+        public override int EndeX { get { return Console.WindowWidth / 3 * 2 - 1; } }
+        public override int EndeY { get { return Console.WindowHeight / 2 -1; } }
 
-        public static int AktuellX { get { return _aktuellX; } set { _aktuellX = value; } }
+        public override int AktuellX { get { return _aktuellX; } set { _aktuellX = value; } }
 
-        public static int AktuellY { get { return _aktuellY; } set { _aktuellY = value; } }
-
-
-        public static void EinstellenAusgabeInformation(List<string> texte)
-        {
-            AktuellY -= texte.Count;
-
-            foreach (string text in texte)
-            {
-                Console.SetCursorPosition(AktuellX, AktuellY);
-                Console.Write(text);
-                AktuellY += 1;
-            }
-        }
-        public static void Reset()
-        {
-            AktuellX = StartX;
-            AktuellY = StartY;
-            Console.SetCursorPosition(AktuellX, AktuellY);
-
-            for (int i = 0; i < EndeY; i++)
-            {
-                Console.WriteLine("                                                                                                             ");
-            }
-            AktuellX = StartX;
-            AktuellY = EndeY-1;
-
-        }
+        public override int AktuellY { get { return _aktuellY; } set { _aktuellY = value; } }
     }
-
-
 }
