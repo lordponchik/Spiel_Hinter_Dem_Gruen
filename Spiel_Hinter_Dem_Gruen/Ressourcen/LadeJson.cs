270f6653a5d46565cc1786359380b55311641b5f
Denys Strashniuk <d.strashniuk@gmail.com>
2025-05-24 22:03:38 +0200

    hinzufügen: Klassen LadeJson und SpielNameDaten

---
+++ Spiel_Hinter_Dem_Gruen/Ressourcen/LadeJson.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Spiel_Hinter_Dem_Gruen.Ressourcen
+{
+    public static class LadeJson
+    {
+        public static SpielNameDaten LadenSpielName()
+        {
+            string pfad = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TextAssets", "SpielName.json");
+
+            string json = File.ReadAllText(pfad);
+
+            return JsonSerializer.Deserialize<SpielNameDaten>(json)!;
+        }
+    }
+}
---
+++ Spiel_Hinter_Dem_Gruen/Ressourcen/SpielNameDaten.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spiel_Hinter_Dem_Gruen.Ressourcen
+{
+   public class SpielNameDaten
+    {
+        public string[][] SpielTitel { get; set; } = Array.Empty<string[]>();
+        public string SubTitle { get; set; } = string.Empty;
+    }
+}
