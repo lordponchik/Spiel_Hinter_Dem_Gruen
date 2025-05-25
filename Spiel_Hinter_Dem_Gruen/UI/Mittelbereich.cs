cba2326c2297189c30c7e6531d748f5060bfa972
Denys Strashniuk <d.strashniuk@gmail.com>
2025-05-25 16:52:31 +0200

    hinzufügen: Klasse Mittelbereich

---
+++ Spiel_Hinter_Dem_Gruen/UI/Mittelbereich.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spiel_Hinter_Dem_Gruen.UI
+{
+    class Mittelbereich : ZeichenbereichBase
+    {
+        private int _aktuellX = 0;
+        private int _aktuellY = Console.WindowHeight / 2;
+        public override int StartX { get { return 0; } }
+        public override int StartY { get { return Console.WindowHeight / 2; } }
+
+        public override int EndeX { get { return Console.WindowWidth / 3 * 2 - 1; } }
+        public override int EndeY { get { return Console.WindowHeight - Console.WindowHeight / 2 / 2 + 1; } }
+
+        public override int AktuellX { get { return _aktuellX; } set { _aktuellX = value; } }
+
+        public override int AktuellY { get { return _aktuellY; } set { _aktuellY = value; } }
+    }
+}
