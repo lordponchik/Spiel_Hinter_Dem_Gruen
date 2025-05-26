00890aa663ddba3037d8bfe46ccc708d77719c7f
Denys Strashniuk <d.strashniuk@gmail.com>
2025-05-26 16:34:26 +0200

    hinzufügen: Klasse Ruestung

---
+++ Spiel_Hinter_Dem_Gruen/Items/Ruestung.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spiel_Hinter_Dem_Gruen.Items
+{
+    class Ruestung : Item
+    {
+        public int Verteidigungswert { get; set; }
+
+        public Ruestung(string name, int verkaufspreis, int kaufpreis, int verteidigungswert) : base(name, "Ruestung", verkaufspreis, kaufpreis)
+        {
+            Verteidigungswert = verteidigungswert;
+        }
+
+        public override Item Klonen()
+        {
+            return new Ruestung(Name, Verkaufspreis, Kaufpreis, Verteidigungswert);
+        }
+
+    }
+}
