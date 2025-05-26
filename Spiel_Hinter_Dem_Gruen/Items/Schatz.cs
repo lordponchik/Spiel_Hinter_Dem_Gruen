fbdbeeb3461948f90f8958a188d06e17bb2aed4b
Denys Strashniuk <d.strashniuk@gmail.com>
2025-05-26 16:34:42 +0200

    hinzufügen: Klasse Schatz

---
+++ Spiel_Hinter_Dem_Gruen/Items/Schatz.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spiel_Hinter_Dem_Gruen.Items
+{
+    class Schatz : Item
+    {
+        public Schatz(string name, int verkaufspreis, int kaufpreis) : base(name, "Schatz", verkaufspreis, kaufpreis){}
+        public override Item Klonen()
+        {
+            return new Schatz(Name, Verkaufspreis, Kaufpreis);
+        }
+    }
+}
