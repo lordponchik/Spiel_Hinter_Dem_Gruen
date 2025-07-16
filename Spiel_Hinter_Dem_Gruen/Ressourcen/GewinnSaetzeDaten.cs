846a6c48d5d482196d5bf36330396cb5a5b6461f
Denys Strashniuk <d.strashniuk@gmail.com>
2025-07-16 19:21:47 +0200

    hinzufügen: Klasse GewinnSaetzeDaten

---
+++ Spiel_Hinter_Dem_Gruen/Ressourcen/GewinnSaetzeDaten.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spiel_Hinter_Dem_Gruen.Ressourcen
+{
+   public class GewinnSaetzeDaten
+    {
+        public string[] Saetze { get; set; } = Array.Empty<string>();
+    }
+}
