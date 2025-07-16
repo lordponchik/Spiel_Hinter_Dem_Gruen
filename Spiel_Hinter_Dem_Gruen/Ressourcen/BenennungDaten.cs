2b339ac7b3101b22402e3692b16b93a0202b0ac1
Denys Strashniuk <d.strashniuk@gmail.com>
2025-07-16 19:21:16 +0200

    hinzufügen: Klasse BenennungDaten

---
+++ Spiel_Hinter_Dem_Gruen/Ressourcen/BenennungDaten.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spiel_Hinter_Dem_Gruen.Ressourcen
+{
+    class BenennungDaten
+    {
+        public List<string> BenennungNachricht { get; set; } = new List<string>();
+    }
+}
