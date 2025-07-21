8e068edfbc696bda92f0307530cf41a48008fe59
Denys Strashniuk <d.strashniuk@gmail.com>
2025-07-21 10:14:40 +0200

    refaktorieren: Klasse Gegner

--- Spiel_Hinter_Dem_Gruen/Spiel/Gegner.cs
+++ Spiel_Hinter_Dem_Gruen/Spiel/Gegner.cs
@@ -13,48 +13,39 @@ namespace Spiel_Hinter_Dem_Gruen.Spiel
     class Gegner : Kaempfer
     {
         private static Mittelbereich _mittelbereich = new Mittelbereich();
-
         private static readonly string[] Koerperteile = { "Kopf", "Rumpf", "Beine" };
-        private static Random _zufall = new Random();
-
+        private static readonly Random _zufall = new Random();
         private List<Item> _auszeichnung = new List<Item>();
+
         public List<Item> Auszeichnung { get { return _auszeichnung; } }
         public List<string> Sprechzeilen { get; }
 
         public Gegner(string name, int leben, int schaden, List<string> sprechzeilen, Waffe? waffe = null) : base(name, leben, schaden, waffe)
         {
             Sprechzeilen = sprechzeilen;
-            AuszeichnungenHinzufuegen();
-        }
-        public override void WaehleAngriff()
-        {
-            KoerperTeilAngriff = _zufall.Next(0, Koerperteile.Length);
-        }
-        public override void WaehleVerteidigung()
-        {
-            KoerperTeilVerteidigung = _zufall.Next(0, Koerperteile.Length);
+            ErzeugeAuszeichnungen();
         }
 
-        public void AuszeichnungenHinzufuegen()
+        public override void WaehleAngriff() => KoerperTeilAngriff = _zufall.Next(0, Koerperteile.Length);
+
+        public override void WaehleVerteidigung() => KoerperTeilVerteidigung = _zufall.Next(0, Koerperteile.Length);
+
+        public void ErzeugeAuszeichnungen()
         {
             _auszeichnung.Clear();
 
             if (AktiveWaffe != null) _auszeichnung.Add(AktiveWaffe);
 
-            int heilmittelAnzahl = _zufall.Next(3, 6);
+            Heilmittel heilmittel = (Heilmittel)ItemDatenbank.zitterpilz.Klonen();
 
-            for (int i = 0; i < heilmittelAnzahl; i += 1)
-            {
-                _auszeichnung.Add(ItemDatenbank.zitterpilz);
-            }
+            heilmittel.Anzahl = _zufall.Next(3, 9);
+
+            _auszeichnung.Add(heilmittel);
         }
-
         public override void Rede()
         {
             _mittelbereich.Reset();
             _mittelbereich.EinstellenAusgabeInformation(Sprechzeilen);
         }
-
-
     }
 }
