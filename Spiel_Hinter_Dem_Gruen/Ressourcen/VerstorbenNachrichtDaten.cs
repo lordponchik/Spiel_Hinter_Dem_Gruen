65925e2bcb332735f06a0c8739b5d87a47ce9607
Denys Strashniuk <d.strashniuk@gmail.com>
2025-07-22 08:22:50 +0200

    hinzufügen: Klasse VersorbenNachrichtDaten und JSON Datei AngabenZumVerstorbenen

---
+++ Spiel_Hinter_Dem_Gruen/Ressourcen/VerstorbenNachrichtDaten.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spiel_Hinter_Dem_Gruen.Ressourcen
+{
+    class VerstorbenNachrichtDaten
+    {
+        public List<string> bild { get; set; } = new List<string>();
+        public List<string> nachricht { get; set; } = new List<string>();
+    }
+}
